0e907a9a5d01a2d8cb67e6b22cd642723173b139
Krish Penumarty <krishpenumarty99@gmail.com>
2016-09-18 18:10:18 -0700

    Added logo, added resume information.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,26 @@ $( document ).ready(function() {
     $( ".nav-menu" ).click(function() {
       toggleMenu();
     });
+
+    $(".animate-arrow").hover(function(){
+        $(this).stop().animate({marginTop: '+10px'}, 250);
+    }, function() {
+        $(this).stop().animate({marginTop: '-10px'}, 250);
+    });
+
+    $(function(){
+        $(".animate-arrow").click(function(){
+           window.location.href="#introduction";
+
+           /* need to stop the form sending of the form
+
+            UPDATE as comment: This may not be exactly correct syntax
+            for stopping a form , look up preventing form submission */
+
+           e.preventDefault();
+           e.stopPropagation();
+        });
+    });
 });
 
 //Toggles the visibility of the menu
@@ -30,8 +50,8 @@ function toggleLogo(color){
     var logo = document.getElementById("logo");
 
     if(color == 'red'){
-        logo.src = "/images/logored.png";
+        logo.src = "/images/krish-logo-2196F3.svg";
     } else {
-        logo.src = "/images/logo.png";
+        logo.src = "/images/krish-logo.svg";
     }
 }

fc85259d159c001161a933b3d8479d50b01e97d0
Krish Penumarty <krishpenumarty99@gmail.com>
2016-09-19 17:50:01 -0700

    Followed Udacity Project Rubric and Code comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,44 @@
 $( document ).ready(function() {
+    // Mouse over function to change color for logo
     $( ".logo" ).mouseover(function() {
-        toggleLogo('red');
+        toggleLogo("red");
     });
 
     $( ".logo" ).mouseout(function() {
-      toggleLogo('white');
+        toggleLogo("white");
     });
 
+    //Ability to toggle Meu on mobile screens
     $( ".nav-menu" ).click(function() {
-      toggleMenu();
+        toggleMenu();
     });
 
+    //Animate arrow downward when user hovers over it.
     $(".animate-arrow").hover(function(){
-        $(this).stop().animate({marginTop: '+10px'}, 250);
+        $(this).stop().animate({marginTop: "+10px"}, 250);
     }, function() {
-        $(this).stop().animate({marginTop: '-10px'}, 250);
+        $(this).stop().animate({marginTop: "-10px"}, 250);
     });
 
+    //OnClick function of arrow to direct user to introduction section
     $(function(){
         $(".animate-arrow").click(function(){
-           window.location.href="#introduction";
-           e.preventDefault();
-           e.stopPropagation();
+            window.location.href="#introduction";
+            e.preventDefault();
+            e.stopPropagation();
         });
     });
 
+    //On click on a menu list item toggles the menu back up
     $(function(){
         $(".menu ul li").click(function(){
             toggleMenu();
         });
-    })
+    });
+
 });
 
-//Toggles the visibility of the menu
+//Function: Toggles the visibility of the menu
 function toggleMenu(){
 
     var nav = $('#navbar-main');
@@ -44,7 +50,7 @@ function toggleMenu(){
     }
 }
 
-//Toggles the color of the logo when mouseover
+//Function: Toggles the color of the logo when mouseover
 function toggleLogo(color){
 
     var logo = document.getElementById("logo");

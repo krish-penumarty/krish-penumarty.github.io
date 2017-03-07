935fb78e8a48511f723dafa597d6d039ef75b520
Krish Penumarty <krishpenumarty99@gmail.com>
2017-03-06 19:43:32 -0700

    Fix for mobile nav bar not closing after linking nav element.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -87,6 +87,22 @@ $(document).ready(function() {
     }
   });
 
+  /* Close mobile nav on click of link */
+  $('.js--main-nav a').click(function(){
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    if ($(window).width() < 768){
+      nav.slideToggle(200);
+    }
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
 }); //End of Document ready
 
 
